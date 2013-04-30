f75e4d57c9a1dea835b84983cbccfee45a5c03d5
Dasfaust <dasfaust@survivorserver.com>
2013-04-30 00:21:04 -0400

    Started work on the requests server

--- src/main/java/com/survivorserver/GlobalMarket/MarketServer.java
+++ src/main/java/com/survivorserver/GlobalMarket/MarketServer.java
@@ -1,15 +1,18 @@
 package com.survivorserver.GlobalMarket;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.net.ServerSocket;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-
-/*import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;*/
+import java.util.jar.JarFile;
 
 public class MarketServer extends Thread {
 	
@@ -23,75 +26,33 @@ public class MarketServer extends Thread {
 		this.storage = storage;
 		this.market = market;
 		enabled = market.getConfig().getBoolean("server.enable");
+		try {
+			loadLibraries();
+		} catch(Exception e) {
+			market.log.warning("Could not load Jackson: " + e.getMessage());
+			e.printStackTrace();
+			enabled = false;
+			return;
+		}
 		sessions = new HashMap<String, UUID>();
 	}
 
 	@Override
 	public void run() {
-		/*try {
+		try {
 			socket = new ServerSocket(6789);
 		} catch (IOException e) {
 			market.log.warning("Could not start server: " + e.getMessage());
 			closeSocket();
-		}*/
+		}
 		while(enabled) {
 			try {
-				socket = new ServerSocket(6789);
+				new ServerHandler(this, socket.accept(), market).start();
 			} catch (IOException e) {
-				market.log.warning("Could not start server: " + e.getMessage());
-				closeSocket();
+				market.log.warning("Could not accep client socket: " + e.getMessage());
 			}
-			try {
-				tickServer();
-			} catch(Exception e) {
-				market.log.warning("Could not tick server: " + e.getMessage());
-				closeSocket();
-				continue;
-			}
-			closeSocket();
 		}
 		closeSocket();
-	}
-	
-	public void tickServer() throws Exception {
-	/*	Socket connection = socket.accept();
-		BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-		DataOutputStream out = new DataOutputStream(connection.getOutputStream());
-		String recieved = in.readLine();
-		market.getLogger().info("Request: " + recieved);
-		Map<String, Object> reply = new HashMap<String, Object>();
-		ObjectMapper mapper = new ObjectMapper();
-		JsonFactory factory = mapper.getFactory();
-		JsonParser parser = null;
-		try {
-			parser = factory.createJsonParser(recieved);
-		} catch (Exception e) {
-			reply.put("failure", "JSON error");
-		}
-		if (parser != null) {
-			JsonNode node = null;
-			try {
-				node = mapper.readTree(parser);
-			} catch (Exception e) {
-				reply.put("failure", "JSON error");
-			}
-			if (node != null) {
-				String function = node.get("function").asText();
-				List<String> args = node.findValuesAsText("args");
-				if (function.equalsIgnoreCase("getAllListings")) {
-					reply.put("success", storage.getAllListings());
-				} else if (function.equalsIgnoreCase("getSessionId")) {
-					reply.put("success", getSessionId(args.get(0)));
-				} else if (function.equalsIgnoreCase("generateSessionId")) {
-					reply.put("success", generateSessionId(args.get(0)));
-				} else if (function.equalsIgnoreCase("getAllMail")) {
-					reply.put("success", storage.getAllMailFor(args.get(0)));
-				} else {
-					reply.put("failure", "Function " + function + " not found");
-				}
-			}
-		}
-		out.writeBytes(new ObjectMapper().writeValueAsString(reply) + "\n");*/
 	}
 	
 	public String sendMailToInventory() {
@@ -127,5 +88,28 @@ public class MarketServer extends Thread {
 	public void setDisabled() {
 		enabled = false;
 		closeSocket();
+	}
+	
+	public void loadLibraries() throws Exception {
+		List<JarFile> libraries = new ArrayList<JarFile>();
+		File core = new File("lib/jackson-core-2.1.4.jar");
+		File databind = new File("lib/jackson-databind-2.1.4.jar");
+		File annotations = new File("lib/jackson-annotations-2.1.4.jar");
+		if (!core.exists() || !databind.exists() || !annotations.exists()) {
+			throw new FileNotFoundException();
+		}
+		libraries.add(new JarFile(core));
+		libraries.add(new JarFile(databind));
+		libraries.add(new JarFile(annotations));
+		for (JarFile library : libraries) {
+	        URL[] urls = { new URL("jar:" + new File(library.getName()).toURI().toURL().toExternalForm() + "!/") };
+	        for (int i = 0; i < urls.length; i++) {
+	        	URLClassLoader sysloader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+		        Class<URLClassLoader> sysclass = URLClassLoader.class;
+		        Method method = sysclass.getDeclaredMethod("addURL", new Class[] { URL.class });
+	            method.setAccessible(true);
+	            method.invoke(sysloader, new Object[] { urls[i] });
+	        }
+		}
 	}
 }
