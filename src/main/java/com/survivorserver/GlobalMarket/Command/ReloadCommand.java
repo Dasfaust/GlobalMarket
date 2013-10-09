4f0f1810ba51de0e6b39febd436d2176b86c6ef8
Dasfaust <ccaudill23@gmail.com>
2013-10-08 21:49:54 -0400

    Update infinite seller on config reload

--- src/main/java/com/survivorserver/GlobalMarket/Command/ReloadCommand.java
+++ src/main/java/com/survivorserver/GlobalMarket/Command/ReloadCommand.java
@@ -1,9 +1,14 @@
 package com.survivorserver.GlobalMarket.Command;
 
+import java.util.Collection;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
+import com.survivorserver.GlobalMarket.Listing;
 import com.survivorserver.GlobalMarket.LocaleHandler;
 import com.survivorserver.GlobalMarket.Market;
 import com.survivorserver.GlobalMarket.SQL.AsyncDatabase;
@@ -48,6 +53,7 @@ public class ReloadCommand extends SubCommand {
 		String db = conf.getString("storage.mysql_database");
 		String addr = conf.getString("storage.mysql_address");
 		int port = conf.getInt("storage.mysql_port");
+		final String infiniteSeller = conf.getString("infinite.seller");
 		market.reloadConfig();
 		conf = market.getConfig();
 		if (!storageType.equalsIgnoreCase(conf.getString("storage.type"))
@@ -68,7 +74,19 @@ public class ReloadCommand extends SubCommand {
 			asyncDb.close();
 			market.initializeStorage();
 		}
-		market.infiniteSeller = market.getConfig().getString("infinite.seller");
+		if (!infiniteSeller.equalsIgnoreCase(conf.getString("infinite.seller"))) {
+			market.infiniteSeller = market.getConfig().getString("infinite.seller");
+			Collection<Listing> infinite = Collections2.filter(market.getStorage().getAllListings(), new Predicate<Listing>() {
+				@Override
+				public boolean apply(Listing listing) {
+					return listing.getSeller().equalsIgnoreCase(infiniteSeller);
+				}
+			});
+			for (Listing listing : infinite) {
+				listing.seller = market.getInfiniteSeller();
+			}
+			market.getInterfaceHandler().updateAllViewers();
+		}
 		market.getConfigHandler().reloadLocaleYML();
 		locale.setSelected();
 		market.buildWorldLinks();
