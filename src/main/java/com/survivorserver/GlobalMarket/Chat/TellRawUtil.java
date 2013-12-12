058daafb1590ccab27ac60b6318128eba05ad5bc
Dasfaust <ccaudill23@gmail.com>
2013-12-12 02:09:40 -0500

    Don't print JSON to console when announcing messages

--- src/main/java/com/survivorserver/GlobalMarket/Chat/TellRawUtil.java
+++ src/main/java/com/survivorserver/GlobalMarket/Chat/TellRawUtil.java
@@ -10,7 +10,6 @@ public class TellRawUtil {
 	public static void announce(Plugin plugin, TellRawMessage message) {
 		try {
 			String json = new net.minecraft.util.com.google.gson.Gson().toJson(message);
-			plugin.getLogger().info(json);
 			net.minecraft.server.v1_7_R1.IChatBaseComponent comp = net.minecraft.server.v1_7_R1.ChatSerializer.a(json);
 			for (World world : plugin.getServer().getWorlds()) {
 				for (Player player : world.getPlayers()) {
