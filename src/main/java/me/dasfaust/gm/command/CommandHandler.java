ce86a6c815fe313d6ce09f2351e370c337a840e4
Dasfaust <ccaudill23@gmail.com>
2016-03-09 02:58:59 -0500

    CommandHandler improvements, why was I using events?! Should fix #70

--- src/main/java/me/dasfaust/gm/command/CommandHandler.java
+++ src/main/java/me/dasfaust/gm/command/CommandHandler.java
@@ -3,7 +3,6 @@ package me.dasfaust.gm.command;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 import me.dasfaust.gm.Core;
 import me.dasfaust.gm.command.cmds.CreateSeverListingCommand;
@@ -15,26 +14,19 @@ import me.dasfaust.gm.menus.Menus;
 import me.dasfaust.gm.tools.GMLogger;
 import me.dasfaust.gm.tools.LocaleHandler;
 
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.CommandMap;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.defaults.BukkitCommand;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerCommandPreprocessEvent;
-import org.bukkit.event.server.ServerCommandEvent;
-
 
 public class CommandHandler implements Listener
 {
 	public static List<CommandContext> commands = new ArrayList<CommandContext>();
 	
-	public CommandHandler()
-	{
-		Core.instance.getServer().getPluginManager().registerEvents(this, Core.instance);
-	}
+	public CommandHandler() { }
 	
 	public void init()
 	{
@@ -45,30 +37,41 @@ public class CommandHandler implements Listener
 			@Override
 			public boolean execute(CommandSender sender, String commandLabel, String[] args)
 			{
-				if (sender instanceof Player)
-				{
-					if (args.length == 1 && sender.hasPermission("globalmarket.viewother"))
-					{
-						String player = args[0];
-						UUID uuid = Core.instance.storage().findPlayer(player);
-						if (uuid == null)
-						{
-							sender.sendMessage(ChatColor.RED + String.format("No player by the name of %s found.", player));
-							return true;
-						}
-						Core.instance.handler().initViewer((Player) sender, uuid, Menus.MENU_LISTINGS);
-					}
-					else
-					{
-						Player player = (Player) sender;
-						Core.instance.handler().initViewer(player, Menus.MENU_LISTINGS);
-					}
-				}
-				else
-				{
-					// Help should be first
-					commands.get(0).process(sender, null);
-				}
+                // Checking up on Bukkit
+                if (!sender.hasPermission("globalmarket.use"))
+                {
+                    sender.sendMessage(LocaleHandler.get().get("command_no_permission"));
+                    return true;
+                }
+                if (args.length > 0)
+                {
+                    String allArgs = StringUtils.join(args, " ");
+                    contextLoop: for (CommandContext context : commands)
+                    {
+                        for (String cmd : context.command)
+                        {
+                            if (allArgs.startsWith(cmd))
+                            {
+                                String _args = allArgs.replace(cmd, "");
+                                handleCommand(sender, context, _args.length() == 0 ? new String[0] : _args.split(" "));
+                                break contextLoop;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if (sender instanceof Player)
+                    {
+                        Player player = (Player) sender;
+                        Core.instance.handler().initViewer(player, Menus.MENU_LISTINGS);
+                    }
+                    else
+                    {
+                        // Help should be first
+                        commands.get(0).process(sender, null);
+                    }
+                }
 				return true;
 			}
 			
@@ -94,54 +97,6 @@ public class CommandHandler implements Listener
 		commands.add(new ReloadCommand());
 		commands.add(new SendCommand());
 		commands.add(new CreateSeverListingCommand());
-	}
-	
-	@EventHandler
-	public void onServer(ServerCommandEvent event)
-	{
-		String message = event.getCommand();
-		String wholeCommand = message.replace("/", "");
-		for (CommandContext context : commands)
-		{
-			for (String cmd : context.command)
-			{
-				String prefix = Core.instance.config().get(Defaults.COMMAND_ROOT_NAME) + " " + cmd;
-				if (wholeCommand.startsWith(prefix))
-				{
-					String _cmd = wholeCommand.replace(prefix, "").replace(cmd, "").trim();
-					GMLogger.debug("Command pre-process server: " + _cmd + " Length: " + _cmd.length());
-					String[] arguments = _cmd.length() == 0 ? new String[0] : _cmd.split(" ");
-					handleCommand(event.getSender(), context, arguments);
-					break;
-				}
-			}
-		}
-	}
-	
-	@EventHandler
-	public void onPlayer(PlayerCommandPreprocessEvent event)
-	{
-		String message = event.getMessage();
-		if (message.startsWith("/"))
-		{
-			String wholeCommand = message.replace("/", "");
-			for (CommandContext context : commands)
-			{
-				for (String cmd : context.command)
-				{
-					String prefix = Core.instance.config().get(Defaults.COMMAND_ROOT_NAME) + " " + cmd;
-					if (wholeCommand.startsWith(prefix))
-					{
-						event.setCancelled(true);
-						String _cmd = wholeCommand.replace(prefix, "").replace(cmd, "").trim();
-						GMLogger.debug("Command pre-process: " + _cmd + " Length: " + _cmd.length());
-						String[] arguments = _cmd.length() == 0 ? new String[0] : _cmd.split(" ");
-						handleCommand(event.getPlayer(), context, arguments);
-						break;
-					}
-				}
-			}
-		}
 	}
 	
 	public void handleCommand(CommandSender sender, CommandContext context, String[] arguments)
